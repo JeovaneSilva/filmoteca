bff84f1efb0a5bb4aef3e95ffca79f6bf98329ac
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-05-13 14:51:04 -0300

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className="flex flex-col items-center">{children}</body>
     </html>
   );
 }
--- src/components/CardFilme.jsx
+++ src/components/CardFilme.jsx
@@ -5,11 +5,11 @@ const ImageURL = process.env.NEXT_PUBLIC_IMG
 const CardFilme = ({filme}) => {
   return (
     <div className='mb-10'>
-        <img src={ImageURL+filme.poster_path} className='w-[290px] h-[330px]'/>
-        <div className='bg-[#fbbd01]'>
-          <h3>{filme.title}</h3>
+        <img src={ImageURL+filme.poster_path} className='w-[300px] h-[330px]'/>
+        <div className='bg-[#fbbd01] flex flex-col justify-center items-center'>
+          <h3 className='text-white font-bold text-xl w-[240px] text-center'>{filme.title}</h3>
           <p>Avaliação: {filme.vote_average}</p>
-          <button className='bg-[#8121ed]'>Ver mais</button>
+          <button className='bg-[#8121ed] w-[100px]'>Ver mais</button>
         </div>
         
     </div>
