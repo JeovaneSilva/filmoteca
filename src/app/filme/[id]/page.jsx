1b7e0c6141327a002368d97b783b63978e005345
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-05-14 16:44:55 -0300

    update projeto

--- src/app/filme/[id]/page.jsx
+++ src/app/filme/[id]/page.jsx
@@ -1,8 +1,47 @@
+"use client"
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+
+const FilmesURL = process.env.NEXT_PUBLIC_API
+const chaveAPI = process.env.NEXT_PUBLIC_API_KEY
+const ImageURL = process.env.NEXT_PUBLIC_IMG
 
 export default function Filme({ params }) {
+
+  const {id} = useParams()
+  const [filme, setFilme] = useState(null)
+
+  const getFilme = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json()
+    setFilme(data)
+  }
+
+  useEffect(() => {
+    const FilmeURL = `${FilmesURL}${id}?${chaveAPI}`
+
+    getFilme(FilmeURL)
+  }, [])
+
+
     return (
-      <main>
-        <h1>filme do id {params.id}</h1>
+      <main className="flex w-full">
+        {filme && (
+          <section className="w-full flex">
+            <div className="w-[30%]">
+              <img src={ImageURL+filme.poster_path} className=' w-full h-[89vh]'/>
+            </div>
+            <div className="w-[70%]">
+              <h1> Titulo: {filme.title}</h1>
+              <h3>Slogan: {filme.tagline}</h3>
+              <p>Orçamento: {filme.budget}</p>
+              <p>Receita: {filme.revenue}</p>
+              <p>Duração: {filme.runtime} min</p>
+              <p>Descrição: {filme.overview}</p>
+            </div>
+            
+          </section>
+        )}
       </main>
     );
   }
--- src/components/CardFilme.jsx
+++ src/components/CardFilme.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { FaRegStar } from "react-icons/fa";
 
 const ImageURL = process.env.NEXT_PUBLIC_IMG
@@ -10,8 +11,11 @@ const CardFilme = ({filme}) => {
         <div className=" flex justify-center items-center flex-col opacity-0 transition-[0.5s] top-0 left-0 absolute w-full h-full bg-[#8021ed50] text-white hover:opacity-[1]">
           <h1 className="w-[90%] text-center text-lg font-bold">{filme.title}</h1>
           <p className="flex justify-center items-center gap-2 mt-3"> <FaRegStar className="text-[#fbbd01] text-2xl"/> {filme.vote_average}</p>
-          <button className="w-[100px] h-[40px] rounded-[10px] font-bold mt-3 bg-[#fbbd01] ">Saber Mais</button>
+          <Link href={`/filme/${filme.id}`}>
+            <button className="w-[100px] h-[40px] rounded-[10px] font-bold mt-3 bg-[#fbbd01] ">Saber Mais</button>
+          </Link>
         </div>
+  
         
 
     </div>
